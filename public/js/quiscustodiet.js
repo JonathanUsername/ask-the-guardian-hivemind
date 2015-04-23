577da18c163858ca5d6dd770f9e513653abd40b8
JonathanUsername <jonathan.j.king@gmail.com>
2015-04-23 22:37:24 +0100

    trying blur fix and timeout fix

--- public/js/quiscustodiet.js
+++ public/js/quiscustodiet.js
@@ -132,24 +132,26 @@ $(document).ready(function(){
 		window.location.hash = params_for_sharing
 		$("#question").blur()
 		$(".start_box").hide()
-		resizeCanvas()
-		$(".fb-like").attr("data-href", window.location.href)
-		$("a.share-url").attr("href", window.location.href).text(window.location.href)
-		// Section cannot be left empty
-		$("#section").val() == "all" ? delete params.section : false;
-		qs = "?" + $.param(params)
-		$.ajax({
-			url:"/search" + qs
-		}).done(function(data){
-			if (data.array.length == 0){
-				alert("No results!")
-			} else {
-				displayCloud(data.array, fetchArticles, params)
-	    	}
-		}).fail(function(jqXHR,textStatus,errorThrown){
-			console.log(jqXHR,textStatus,errorThrown)
-			alert(textStatus,errorThrown,"Error connecting to server")
-		})
+		window.setTimeout(function(){
+			resizeCanvas()
+			$(".fb-like").attr("data-href", window.location.href)
+			$("a.share-url").attr("href", window.location.href).text(window.location.href)
+			// Section cannot be left empty
+			$("#section").val() == "all" ? delete params.section : false;
+			qs = "?" + $.param(params)
+			$.ajax({
+				url:"/search" + qs
+			}).done(function(data){
+				if (data.array.length == 0){
+					alert("No results!")
+				} else {
+					displayCloud(data.array, fetchArticles, params)
+		    	}
+			}).fail(function(jqXHR,textStatus,errorThrown){
+				console.log(jqXHR,textStatus,errorThrown)
+				alert(textStatus,errorThrown,"Error connecting to server")
+			})
+		},500)
 	}
 
 	function displayCloud(array, clickHandle, params){
